0857eef21edbbebb08ab779c5bc20f850d77dbd3
unknown <alphasquad42@gmail.com>
2016-03-29 19:34:19 -0700

    Fixed things I previously broke?

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,21 +1,12 @@
 angular.module('app.controllers', ['ionic'])
+    .controller('appCtrl', ['$scope', 'auth', function ($scope, auth, $ionicSideMenuDelegate) {
+        $scope.isLoggedIn = auth.isLoggedIn;
+        $scope.currentUser = auth.currentUser;
+        $scope.logOut = auth.logOut;
 
-// With the new view caching in Ionic, Controllers are only called
-// when they are recreated or on app start, instead of every page change.
-// To listen for when this page is active (for example, to refresh data),
-// listen for the $ionicView.enter event:
-//
-//$scope.$on('$ionicView.enter', function(e) {
-//});
-
-.controller('appCtrl', ['$scope', 'auth', function ($scope, auth, $ionicSideMenuDelegate) {
-    $scope.isLoggedIn = auth.isLoggedIn;
-    $scope.currentUser = auth.currentUser;
-    $scope.logOut = auth.logOut;
-
-    $scope.toggleLeft = function () {
-        $ionicSideMenuDelegate.toggleLeft();
-    };
+        $scope.toggleLeft = function () {
+            $ionicSideMenuDelegate.toggleLeft();
+        };
 }])
 
 .controller('authCtrl', ['$scope', '$state', 'auth', function ($scope, $state, auth) {
@@ -117,7 +108,6 @@ angular.module('app.controllers', ['ionic'])
         }
     };
 
-<<<<<<< HEAD
     $scope.ratingsCallback = function (rating) {
         $scope.data = {
             ratings: rating
@@ -127,24 +117,6 @@ angular.module('app.controllers', ['ionic'])
     $scope.submitRating = function () {
         tours.setRate($location.path().split("/")[4], $scope.data).success(function () {
             $scope.rated = true;
-=======
-    $scope.showPopup = function () {
-        $scope.buttonDisable = false;
-        $ionicPopup.show({
-            title: 'Rate this tour site',
-            template: '<div class="item range range-balanced"> <i class="icon ion-sad-outline"></i><input type="range" name="rating" min="0" max="5" value="5"><i class="icon ion-happy-outline"></i></div>',
-            buttons: [{ // Array[Object] (optional). Buttons to place in the popup footer.
-                text: 'Cancel',
-                type: 'button-default',
-  }, {
-                text: 'OK',
-                type: 'button-positive',
-                onTap: function (e) {
-                    // Returning a value will cause the promise to resolve with the given value.
-                    return scope.data.response;
-                }
-  }]
->>>>>>> 8656a88ed7f5f7e52bb6c3a72fbe81679e633a9c
         });
         $scope.rated = true;
     };
@@ -178,7 +150,7 @@ angular.module('app.controllers', ['ionic'])
         $scope.modal.hide();
         $scope.modal.remove();
     };
-    
+
     $scope.closetechnicalModal = function () {
         $scope.technicalmodal.hide();
         $scope.technicalmodal.remove();
@@ -284,57 +256,54 @@ angular.module('app.controllers', ['ionic'])
     $scope.getTourMarkers = function () {
         tourmarkers.getTourMarkers().success(function (data) {
             $scope.tourmarkers = data;
+            console.log($scope.tourmarkers);
             drawMarkers();
         });
     };
-<<<<<<< HEAD
-=======
-    
-    $scope.currentStop=0;
-    
+
+    $scope.currentStop = 0;
+
     $scope.nextStop = function () {
         $scope.currentStop++;
         /*marker.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
 		directionsDisplay.setMap(map);
         $scope.map = map;
-		drawMarkers(directionsService, directionsDisplay, marker);*/  
+		drawMarkers(directionsService, directionsDisplay, marker);*/
         // above code will work if line below does not
         onSuccessDrawMarker(position);
         console.log("moving forward" + $scope.currentStop);
-    }
-    
+    };
+
     $scope.prevStop = function () {
         $scope.currentStop--;
         onSuccessDrawMarker(position);
         console.log("moving back" + $scope.currentStop);
-    }
-    
-    ///////////////////Directions Display//////////////////////
-	var directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true});
-	var directionsService = new google.maps.DirectionsService;
-	
->>>>>>> 8656a88ed7f5f7e52bb6c3a72fbe81679e633a9c
+    };
 
-    var drawMarkers = function () {
+    ///////////////////Directions Display//////////////////////
+    var directionsDisplay = new google.maps.DirectionsRenderer({
+        suppressMarkers: true
+    });
+
+    var directionsService = new google.maps.DirectionsService;
+
+    var drawMarkers = function (directionsService, directionsDisplay, marker) {
 
         var markers;
         var content;
-<<<<<<< HEAD
         var infoWindow;
 
-=======
-        var infoWindow;	
+        function rad(x) {
+            return x * Math.PI / 180;
+        }
 
-        function rad(x) {return x*Math.PI/180;}
-		
         var lat = marker.position.lat();
         var lng = marker.position.lng();
-        var R = 6371; 							// radius of earth in km
-		var distances = [];
+        var R = 6371; // radius of earth in km
+        var distances = [];
         var distancesCopy = [];
-		var shortest = -1;
-                
->>>>>>> 8656a88ed7f5f7e52bb6c3a72fbe81679e633a9c
+        var shortest = -1;
+
         for (var i = 0; i < $scope.tourmarkers.length; i++) {
             content = '<h2>' + $scope.tourmarkers[i].title + '</h2>' +
                 '<br />' +
@@ -347,107 +316,56 @@ angular.module('app.controllers', ['ionic'])
             });
 
             var point = new google.maps.LatLng($scope.tourmarkers[i].lat, $scope.tourmarkers[i].lon);
-<<<<<<< HEAD
 
-=======
-            
-            var mlat = point.lat();      
-			var mlng = point.lng();
-            var dLat  = rad(mlat - lat);
-			var dLong = rad(mlng - lng);
-			var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(rad(lat)) * Math.cos(rad(lat)) * Math.sin(dLong/2) * Math.sin(dLong/2);
-			var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-			var d = R * c;
-			
-			distances[i] = d;
+            var mlat = point.lat();
+            var mlng = point.lng();
+            var dLat = rad(mlat - lat);
+            var dLong = rad(mlng - lng);
+            var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(rad(lat)) * Math.cos(rad(lat)) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            var d = R * c;
+
+            distances[i] = d;
             distancesCopy[i] = d;
-            
-			distances.sort();
-            
-            
+
+            distances.sort();
+
+
             var stopKey = distancesCopy.indexOf(distances[$scope.currentStop]);
-            
-            
-				
->>>>>>> 8656a88ed7f5f7e52bb6c3a72fbe81679e633a9c
+
+
+
             markers = new google.maps.Marker({
                 label: "S",
                 position: point,
                 map: map,
                 info: content
-<<<<<<< HEAD
             });
-=======
-			});
-			
->>>>>>> 8656a88ed7f5f7e52bb6c3a72fbe81679e633a9c
+
             //SCOPE: 'this' refers to the current 'markers' object, we pass in the info and marker
             google.maps.event.addListener(markers, 'click', function () {
                 infoWindow.setContent(this.info);
                 infoWindow.open(map, this);
-            });	
+            });
         }
-<<<<<<< HEAD
+        var dest_point_lat = ($scope.tourmarkers[stopKey].lat);
+        var dest_point_lon = ($scope.tourmarkers[stopKey].lon);
+        var dest_end = new google.maps.LatLng(dest_point_lat, dest_point_lon);
 
-    };
-
-    /////////////
-    var closest = function (marker, tourmarkersList) {
-        var pi = Math.PI;
-        var R = 6371; //equatorial radius
-        var distances = [];
-        var tourmarkers = tourmarkersList;
-        var closest = -1;
-        var lat1 = marker.position.lat();
-        var lon1 = marker.position.lng();
-
-        for (i = 0; i < tourmarkers.length; i++) {
-            var current = new google.maps.LatLng(tourmarkers[i].lat, tourmarkers[i].lon);
-            var lat2 = current.lat();
-            var lon2 = current.lng();
-
-            var chLat = lat2 - lat1;
-            var chLon = lon2 - lon1;
-
-            var dLat = chLat * (pi / 180);
-            var dLon = chLon * (pi / 180);
-
-            var rLat1 = lat1 * (pi / 180);
-            var rLat2 = lat2 * (pi / 180);
-
-            var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(rLat1) * Math.cos(rLat2);
-            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-            var d = R * c;
-
-            distances[i] = d;
-            if (closest == -1 || d < distances[closest]) {
-                closest = i;
+        directionsService.route({
+            origin: marker.position,
+            destination: dest_end,
+            travelMode: google.maps.TravelMode.WALKING
+        }, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+            } else {
+                window.alert('Directions request failed due to ' + status);
             }
-        }
+        });
 
-        // (debug) The closest marker is:
-        return (tourmarkers[closest]);
     };
-=======
-		    var dest_point_lat = ($scope.tourmarkers[stopKey].lat);
-			var dest_point_lon = ($scope.tourmarkers[stopKey].lon);
-			var dest_end = new google.maps.LatLng(dest_point_lat, dest_point_lon);
-        
-				directionsService.route({
-					origin: marker.position,  
-				destination: dest_end,               
-					travelMode: google.maps.TravelMode.WALKING
-				}, function(response,status) {
-					if(status==google.maps.DirectionsStatus.OK) {
-						directionsDisplay.setDirections(response);
-					} else {
-						window.alert('Directions request failed due to ' + status);
-					}
-				});
-				
-	};
 
->>>>>>> 8656a88ed7f5f7e52bb6c3a72fbe81679e633a9c
 
     var myLatlng = new google.maps.LatLng(38.5602, -121.4241);
 
@@ -460,64 +378,18 @@ angular.module('app.controllers', ['ionic'])
     };
 
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-<<<<<<< HEAD
 
     var marker = new google.maps.Marker({
-=======
-	
-	var marker = new google.maps.Marker({
-		label: "*",
->>>>>>> 8656a88ed7f5f7e52bb6c3a72fbe81679e633a9c
+        label: "*",
         position: myLatlng,
         map: map,
-        title: 'Sac State'
+        title: 'SAC STATE'
     });
-<<<<<<< HEAD
-
-    ////////////Directions Display/////////////////
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    var directionsService = new google.maps.DirectionsService;
 
 
-    var calculateAndDisplayRoute = function (directionsService, directionsDisplay, marker, dest) {
-        var lat = dest.lat;
-        var lon = dest.lng;
-        directionsService.route({
-            //origin: {lat:38.53, lng: -121.41555},
-            origin: marker.position,
-            //destination: {lat:38.558961, lng: -121.423011},
-            destination: {
-                lat: lat,
-                lng: lon
-            },
-            //destination: dest.position,
-            travelMode: google.maps.TravelMode.WALKING
-        }, function (response, status) {
-            if (status == google.maps.DirectionsStatus.OK) {
-                directionsDisplay.setDirections(response);
-            } else {
-                window.alert('Directions request failed due to ' + status);
-            }
-        });
-    };
 
-
-    ////////////////////////
-=======
->>>>>>> 8656a88ed7f5f7e52bb6c3a72fbe81679e633a9c
-
-	
-	
     var onSuccess = function (position) {
         marker.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-<<<<<<< HEAD
-        directionsDisplay.setMap(map);
-        var dest = new google.maps.Marker({});
-        //dest.setPosition(38.558961,-121.423011);
-        //dest.setPosition((closest(marker, $scope.tourmarkers)).position);  // if you can get this line to work without commenting it out then you're set
-        calculateAndDisplayRoute(directionsService, directionsDisplay, marker, dest);
-=======
->>>>>>> 8656a88ed7f5f7e52bb6c3a72fbe81679e633a9c
         $scope.map = map;
     };
 
@@ -527,22 +399,22 @@ angular.module('app.controllers', ['ionic'])
     }
 
     navigator.geolocation.watchPosition(onSuccess, onError, {
-        maximumAge: 3000,   
-        timeout: 5000,    
+        maximumAge: 3000,
+        timeout: 5000,
         enableHighAccuracy: true
     });
 
 
-///////This new onSuccessDrawMarker and watchPosition refreshes drawMarkers at a slower time to prevent query timeout    
+    ///////This new onSuccessDrawMarker and watchPosition refreshes drawMarkers at a slower time to prevent query timeout    
     var onSuccessDrawMarker = function (position) {
         marker.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-		directionsDisplay.setMap(map);
+        directionsDisplay.setMap(map);
         $scope.map = map;
-		drawMarkers(directionsService, directionsDisplay, marker);
+        drawMarkers(directionsService, directionsDisplay, marker);
     };
-	
-	navigator.geolocation.watchPosition(onSuccessDrawMarker, onError, {
-		maximumAge: 5000,
+
+    navigator.geolocation.watchPosition(onSuccessDrawMarker, onError, {
+        maximumAge: 5000,
         enableHighAcuracy: true
-	});
+    });
 });
